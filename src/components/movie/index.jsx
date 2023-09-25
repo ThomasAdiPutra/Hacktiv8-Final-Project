0f89def4cc92ee708c3eb110010dd7ec56b5a382
Thomas <thomasadiputra@gmail.com>
2023-09-25 20:50:31 +0700

    Change image base uri

--- src/components/movie/index.jsx
+++ src/components/movie/index.jsx
@@ -21,7 +21,7 @@ export default function Movie({
   return (
     <div className="h-[210px] w-[140px] md:h-[263px] md:w-[175px] lg:h-[365px] lg:w-[243px] min-[2560px]:w-[300px] min-[2560px]:h-[500px] rounded-xl overflow-hidden group">
       <img
-        src={`${process.env.REACT_APP_TMDB_IMAGE_BASE_URI}/${poster}`}
+        src={`${process.env.REACT_APP_TMDB_IMAGE_BASE_URI}/w500/${poster}`}
         className="h-full w-full object-cover z-10 group-hover:scale-125 duration-500"
         alt={title}
         onError={(e) => {
